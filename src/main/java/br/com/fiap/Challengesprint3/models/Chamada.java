9843cc91c5c167d3888b70588f5761793b7dc4e8
Claudio Jin <natsu165@hotmail.com>
2022-09-16 14:46:37 -0300

    atibutos e model chamada

--- src/main/java/br/com/fiap/Challengesprint3/models/Chamada.java
+++ src/main/java/br/com/fiap/Challengesprint3/models/Chamada.java
@@ -1,5 +1,11 @@
 package br.com.fiap.Challengesprint3.models;
 
+import lombok.Data;
+
+@Data
 public class Chamada {
-    
+    private Long id;
+    private int codChamada;
+    private int duracaoChamada;
+    private int avaliacao;
 }
