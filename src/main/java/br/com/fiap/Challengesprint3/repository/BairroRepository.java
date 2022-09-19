0b5577a0d6db727859f8261fdaa85af593e27eb8
Claudio Jin <natsu165@hotmail.com>
2022-09-19 17:21:41 -0300

    removido repositories e services

--- src/main/java/br/com/fiap/Challengesprint3/repository/BairroRepository.java
+++
@@ -1,9 +0,0 @@
-// package br.com.fiap.Challengesprint3.repository;
-
-// import org.springframework.data.jpa.repository.JpaRepository;
-
-// import br.com.fiap.Challengesprint3.models.Bairro;
-
-// public interface BairroRepository extends JpaRepository<Bairro, Long>{
-    
-// }
--- src/main/java/br/com/fiap/Challengesprint3/repository/CidadeRepository.java
+++
@@ -1,9 +0,0 @@
-// package br.com.fiap.Challengesprint3.repository;
-
-// import org.springframework.data.jpa.repository.JpaRepository;
-
-// import br.com.fiap.Challengesprint3.models.Cidade;
-
-// public interface CidadeRepository extends JpaRepository<Cidade, Long>{
-    
-// }
